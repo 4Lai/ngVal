392a560209487ea6218965b92b8a655684472f8f
4Lai <krystianzielinski2tor@gmail.com>
2023-08-12 14:55:48 +0200

    bugfix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HeaderSectionHomeComponent } from './shared/components/header-section-home/header-section-home.component';
 import { NavigationMenuComponent } from './shared/components/navigation-menu/navigation-menu.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, HeaderSectionHomeComponent, NavigationMenuComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    HeaderSectionHomeComponent,
+    NavigationMenuComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
